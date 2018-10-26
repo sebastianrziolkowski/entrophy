0590e2650ca89ffd46766c6bfc38581fc1b37801
Sebastian Ziółkowski <s.r.ziolkowski@gmail.com>
2018-10-26 21:17:29 +0200

    Added counting, sort
    -letter counting
    -sort arrayList

--- src/Entropia.java
+++ src/Entropia.java
@@ -1,3 +1,4 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -13,13 +14,38 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-
-
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 
 
 public class Entropia extends Application {
+
+        class charElement
+        {
+            int value=0;
+            char code;
+
+
+
+            public charElement(int i, char Code) {
+               value=i;
+               code=Code;
+            }
+
+
+
+        }
+
+
+        //* https://medium.freecodecamp.org/utilizing-javas-arrays-sort-for-any-list-of-objects-e3e2db61d70b
+        class SortByValue implements Comparator<charElement> {
+            public int compare(charElement a, charElement b) {
+                return b.value - a.value;
+            }
+        }
+
 
     private Text actionStatus;
     private Text resultText;
@@ -54,6 +80,7 @@ public class Entropia extends Application {
         textEdytor.setMinSize(500,25);
 
         open.setOnAction(new SingleFcButtonListener());
+        load.setOnAction(new ActionButtonListener());
 
         HBox textBox = new HBox(10);
         HBox buttonBox = new HBox(10);
@@ -81,6 +108,17 @@ public class Entropia extends Application {
         }
     }
 
+
+    private class ActionButtonListener implements EventHandler<ActionEvent> {
+
+        @Override
+        public void handle(ActionEvent e) {
+
+            countChar();
+        }
+    }
+
+
     private void showSingleFileChooser() {
 
         FileChooser fileChooser = new FileChooser();
@@ -91,7 +129,11 @@ public class Entropia extends Application {
             actionStatus.setText("File selected: " + fileName );
             try {
                 BufferedReader bufor = new BufferedReader(new FileReader(fileName));
-                textEdytor.setText(bufor.readLine());
+
+                while(bufor.ready())
+                {
+                    textEdytor.setText(textEdytor.getText() + bufor.readLine());
+                }
             } catch (Exception e) {
                 System.out.println("Error file load" + e.toString());
             }
@@ -99,6 +141,55 @@ public class Entropia extends Application {
 
 
 
+    }
+
+    private void countChar()
+    {
+        boolean check = false;
+        ArrayList<charElement> charArrayList = new ArrayList<charElement>();
+        try
+        {
+            String text = textEdytor.getText();
+
+            if(charArrayList.isEmpty())
+            {
+                charArrayList.add(new charElement(1,text.charAt(0)));
+            }
+
+            if(!charArrayList.isEmpty())
+            {
+                for(int i=1;i<text.length();i++)
+                {
+                    check=false;
+                   for(int j=0;j<charArrayList.size();j++)
+                   {
+                       if((int)charArrayList.get(j).code==(int)text.charAt(i))
+                       {
+                           check = true;
+                           charArrayList.get(j).value++;
+                       }
+                   }
+                   if(check==false)
+                   {
+                       charArrayList.add(new charElement(1,text.charAt(i)));
+                   }
+                }
+            }
+
+
+            Collections.sort(charArrayList, new SortByValue());
+
+            for(int i=0;i<charArrayList.size();i++)
+            {
+                System.out.println(charArrayList.get(i).code + " -> " + charArrayList.get(i).value);
+            }
+
+
+        }
+        catch (Exception e)
+        {
+            System.out.println("Get text from edytor error" + e.toString());
+        }
     }
 
 }
